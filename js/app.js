15ced8f6604dea7b16a1722b2d312bf1f2209b15
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2016-10-15 20:24:41 -0500

    validacion datos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+var cargarPagina = function() {
+	$("#numero").focus();
+	$("#numero").keydown(deshabilitar);
+	$("#numero").keyup(validacion);
+	$("#siguiente").click(aleatorio);
+	$(".num:first").focus();
+	$("#numero-tel").text(numeroCel);
+	$(".num").keydown(deshabilitarNum);
+	$(".num").keyup(focus);
+	$("#boton").click(nuevoAleatorio);
+	$("#sigVerify").click(validacionCod);
+	$("#sigProf").click(validacionDatos);
+};
+
+$(document).ready(cargarPagina);
+
+var codigoRandom = localStorage.getItem("codigo");
+var codigoRandomDos = localStorage.getItem("codigoDos");
+var numeroCel = localStorage.getItem("numCelular");
+
 var deshabilitar = function(evento) {
 	var ascii = evento.keyCode;
 	if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
@@ -59,28 +79,31 @@ var focus = function(evento) {
 var validacionCod = function() {
 	var codigo = $(".num").eq(0).val() + $(".num").eq(1).val() + $(".num").eq(2).val();
 	var codUno = $(".num").eq(0).val().length;
-	if (codigoRandom == codigo) {
+	if (codigoRandom == codigo || codigoRandomDos == codigo) {
 		$(this).attr("href", "signup2.html");
 	} else if (codUno == 0) {
 		alert("Ingrese su código por favor!");
-	} else if (codigoRandom != codigo) {
+	} else if (codigoRandom != codigo || codigoRandomDos != codigo) {
 		alert("Código Inválido!");
 	}
 };
 
-var cargarPagina = function() {
-	$("#numero").focus();
-	$("#numero").keydown(deshabilitar);
-	$("#numero").keyup(validacion);
-	$("#siguiente").click(aleatorio);
-	$(".num:first").focus();
-	$("#numero-tel").text(numeroCel);
-	$(".num").keydown(deshabilitarNum);
-	$(".num").keyup(focus);
-	$("#sigVerify").click(validacionCod);
+var nuevoAleatorio = function(evento) {
+	evento.preventDefault();
+	var randomDos = Math.floor(Math.random()*900) + 99;
+	alert("LAB - " + randomDos);
+	localStorage.setItem("codigoDos", randomDos);
 };
 
-var codigoRandom = localStorage.getItem("codigo");
-var numeroCel = localStorage.getItem("numCelular");
-
-$(document).ready(cargarPagina);
+var validacionDatos = function() {
+	var nombre = $("#nombre").val().trim().length;
+	var apellido = $("#apellido").val().trim().length;
+	var email = $("#email").val().trim().length;
+	var correo = $("#email").val().trim();
+	var regEx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	if (nombre > 1 && nombre < 20 && apellido > 1 && apellido < 30 && email > 5 && email < 50 && regEx.test(correo)) {
+		$(this).attr("href", "profile.html");
+	} else {
+		alert("Ingresa tus datos correctamente!");
+	}
+};
