fe3a07d2ac79373a4dd064819ea3d397396b7268
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-10-20 11:09:00 -0500

    cambios api image

--- js/app.js
+++ js/app.js
@@ -208,10 +208,7 @@ var error = function(error) {
 };
 
 var capturar = function(event) {
-       if(event.target.files.length == 1 && 
-          event.target.files[0].type.indexOf("image/") == 0) {
-           $("#image").attr("src",URL.createObjectURL(event.target.files[0]));
-       }
-   
-	
+    if(event.target.files.length == 1 && event.target.files[0].type.indexOf("image/") == 0) {
+        $("#image").attr("src", URL.createObjectURL(event.target.files[0]));
+    }	
 };
